19a55911eed0922456ca29a051480871e51c9bb6
Daniel Oluwarotimi <danielo@techexchange.in>
2023-03-07 21:12:43 +0000

    Imlemented unit test to cover all methods in backend class excluding sign up and sign in

--- flaskr/pages.py
+++ flaskr/pages.py
@@ -1,10 +1,14 @@
 from flask import Flask, request, render_template, redirect, session, flash, url_for, send_file
 import os
 from flaskr.backend import Backend
+global set_app
 
-def make_endpoints(app):
-    instance = Backend()
+def make_endpoints(app, mock_app = False,mock_render_template = False):
+    instance = Backend() 
     app.secret_key = b'0490214e639a85e4e47041cde14a56b219c0b10e709e40d9dfafe4a4e46e8807'
+    app = app if mock_app is False else mock_app
+    global render_template
+    render_template if mock_render_template is False else mock_render_template
 
     # Flask uses the "app.route" decorator to call methods when users
     # go to a specific route on the project's website.
--- flaskr/pages_test.py
+++ flaskr/pages_test.py
@@ -1,5 +1,7 @@
 from flaskr import create_app
+from flaskr.pages import Backend
 import unittest 
+from unittest.mock import MagicMock
 import pytest
 
 # See https://flask.palletsprojects.com/en/2.2.x/testing/ 
@@ -9,16 +11,52 @@ import pytest
 class Test_pages: 
     'This includes test to make sure all of the methods for our pages are up and running'
 
+    
     @pytest.fixture(scope="class", autouse=True)
     def app(self):
         app = create_app({
             'TESTING': True,
         })
         return app
+
+    def Make_endpoints(mock_render_template,):
+        make_endpoints(app, mock_render_template)
+    
+    @pytest.fixture(scope="class", autouse=True)
+    def file(self):
+        file = MagicMock()
+        return file
+
+    @pytest.fixture(scope="class", autouse=True)
+    def request(self, file):
+        request = MagicMock()
+        request.file.return_value = file
+        return request
+
+    def render_template(self, req_file,request):
+        request.file()
+        return request.file
+    
+    @pytest.fixture(scope="class", autouse=True)
+    def get(self,request):
+        get= MagicMock()
+        get.return_value = request.file
+        return get 
+
+    def request_1(self,asked_request,data_set, key_giving ,request):
+        request.files = data_set
+        request.form.return_value.get.return_value =  data_set
+        
+                
+
+    #@pytest.fixture(scope="class", autouse=True)
+    #def test_upload_file(self):
+     #   request.method = 
     
     @pytest.fixture(scope="class",  autouse=True)
     def client(self,app):
         return app.test_client()
+
 
     def test_home_page(self,client):  
         with client:
